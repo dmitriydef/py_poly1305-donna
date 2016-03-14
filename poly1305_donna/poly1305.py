bbf364c210302fb5e6dba7bf2859219e273b4c49
Sundar Nagarajan <sun.nagarajan@gmail.com>
2016-03-14 15:25:27 -0700

    Renamed utils to utils2to3. Removed unnecessary class containing only classmethods

--- poly1305_donna/poly1305.py
+++ poly1305_donna/poly1305.py
@@ -1,5 +1,5 @@
 import os
-from utils import toBytes
+from utils2to3 import toBytes
 from cffi_utils.sowrapper import get_lib_ffi_resource
 
 
@@ -22,71 +22,68 @@ libpath = 'libpoly1305donna.so'
     module_name=module_name, libpath=libpath, c_hdr=c_hdr)
 
 
-class Poly1305(object):
-    @classmethod
-    def self_test(cls):
-        '''
-        Returns-->boolean
-        '''
-        return (lib.poly1305_power_on_self_test() == 1)
+def self_test():
+    '''
+    Returns-->boolean
+    '''
+    return (lib.poly1305_power_on_self_test() == 1)
 
-    @classmethod
-    def get_key(cls, random_fn=None):
-        '''
-        random_fn-->callable: takes one int param: number of random bytes
-                    Preferably leave unset; os.urandom() will be used
-        Returns-->bytes: Secure key
-        '''
-        if random_fn is None:
-            random_fn = os.urandom
-        kr = ffi.new('unsigned char[32]', random_fn(32))
-        return ffi.get_bytes(kr)
 
-    @classmethod
-    def authenticate(cls, kr, msg, random_fn=None):
-        '''
-        kr-->bytes: Secure key - returned by get_key()
-        msg-->str or bytes: Message to be authenticated
-        random_fn-->callable: takes one int param: number of random bytes
-                    Preferably leave unset; os.urandom() will be used
-        Returns-->bytes: auth
-        '''
-        if random_fn is None:
-            random_fn = os.urandom
-        auth = ffi.new('unsigned char[16]')
-        msg = toBytes(msg)
+def get_key(random_fn=None):
+    '''
+    random_fn-->callable: takes one int param: number of random bytes
+                Preferably leave unset; os.urandom() will be used
+    Returns-->bytes: Secure key
+    '''
+    if random_fn is None:
+        random_fn = os.urandom
+    kr = ffi.new('unsigned char[32]', random_fn(32))
+    return ffi.get_bytes(kr)
 
-        lib.poly1305_auth(auth, msg, len(msg), kr)
-        return ffi.get_bytes(auth)
 
-    @classmethod
-    def verify(cls, auth, kr, msg, random_fn=None):
-        '''
-        auth-->bytes
-        kr-->bytes: Secure key - returned by get_key()
-        msg-->str or bytes: Message to be authenticated
-        Returns-->boolean
-        '''
-        (auth, kr, msg) = (toBytes(auth), toBytes(kr), toBytes(msg))
-        auth2 = cls.authenticate(kr, msg, random_fn=random_fn)
-        return (lib.poly1305_verify(auth, auth2) == 1)
+def authenticate(kr, msg, random_fn=None):
+    '''
+    kr-->bytes: Secure key - returned by get_key()
+    msg-->str or bytes: Message to be authenticated
+    random_fn-->callable: takes one int param: number of random bytes
+                Preferably leave unset; os.urandom() will be used
+    Returns-->bytes: auth
+    '''
+    if random_fn is None:
+        random_fn = os.urandom
+    auth = ffi.new('unsigned char[16]')
+    msg = toBytes(msg)
+
+    lib.poly1305_auth(auth, msg, len(msg), kr)
+    return ffi.get_bytes(auth)
+
+
+def verify(auth, kr, msg, random_fn=None):
+    '''
+    auth-->bytes
+    kr-->bytes: Secure key - returned by get_key()
+    msg-->str or bytes: Message to be authenticated
+    Returns-->boolean
+    '''
+    (auth, kr, msg) = (toBytes(auth), toBytes(kr), toBytes(msg))
+    auth2 = authenticate(kr, msg, random_fn=random_fn)
+    return (lib.poly1305_verify(auth, auth2) == 1)
 
 
 if __name__ == '__main__':
-    p = Poly1305
-    print('Power-On Self Test result: ' + str(p.self_test()))
+    print('Power-On Self Test result: ' + str(self_test()))
     msg = 'Hello world'
     print('\nTest class methods - authenticate, verify')
-    kr = p.get_key()
-    auth = p.authenticate(kr, msg)
+    kr = get_key()
+    auth = authenticate(kr, msg)
 
-    bad_kr = p.get_key()
+    bad_kr = get_key()
     bad_msg = msg + '1'
-    bad_auth = p.authenticate(kr, bad_msg)
+    bad_auth = authenticate(kr, bad_msg)
 
     print('Good: %s\nBad auth: %s\nBad kr: %s\nBad msg: %s' % (
-        str(p.verify(auth, kr, msg)),
-        str(p.verify(bad_auth, kr, msg)),
-        str(p.verify(auth, bad_kr, msg)),
-        str(p.verify(auth, kr, bad_msg))
+        str(verify(auth, kr, msg)),
+        str(verify(bad_auth, kr, msg)),
+        str(verify(auth, bad_kr, msg)),
+        str(verify(auth, kr, bad_msg))
     ))
