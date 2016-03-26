e1942bebc78e9d04cb0a1b285809cccf05a52ba2
Sundar Nagarajan <sun.nagarajan@gmail.com>
2016-03-25 18:02:01 -0700

    0.11.12

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ PACKAGE DATA
 '''
 # You _SHOULD_ set these
 name = 'poly1305_donna'
-version = '0.11.11'   # oldver: '0.11.10'
+version = '0.11.12'   # oldver: '0.11.11'
 url = 'https://github.com/sundarnagarajan/py_poly1305-donna'
 download_url = '%s/tree/%s' % (url, version)
 description = name
@@ -23,10 +23,6 @@ packages = find_packages()
 license = 'License :: OSI Approved :: MIT License'
 
 # The following are optional
-try:
-    long_description = open('README.rst').read()
-except:
-    long_description = description
 author = 'Sundar Nagarajan'
 # author_email = ''
 maintainer = author
@@ -90,6 +86,19 @@ ADDL_KWARGS = dict(
 '''
 
 
+def get_longdesc(default=''):
+    '''
+    Returns-->str
+    '''
+    files = ['README.rst', 'README.md', 'README.txt', 'README']
+    for f in files:
+        try:
+            return open(f, 'r').read()
+        except:
+            continue
+    return default
+
+
 def get_dirtree(topdir, dirlist=[]):
     '''
     topdir-->str: must be name of a dir under current working dir
@@ -125,6 +134,8 @@ dirlist = locals().get('data_dirs', None)
 if isinstance(dirlist, list):
     package_dir = {name: name}
     package_data = {name: get_dirtree(topdir=name, dirlist=dirlist)}
+
+long_description = get_longdesc(description)
 
 known_keywords = [
     'name', 'version', 'packages', 'description', 'license',
