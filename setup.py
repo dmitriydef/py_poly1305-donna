50f4c8dd9f1b3535af3e5f032cdaada6ebfa829b
Sundar Nagarajan <sun.nagarajan@gmail.com>
2016-03-26 14:08:06 -0700

    No longer install C code

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ c_src_files = [
     'poly1305-donna.c',
 ]
 libpath = os.path.join(name, libname)
-c_src_list = [os.path.join(name, c_dir, x) for x in c_src_files]
+c_src_list = [os.path.join(c_dir, x) for x in c_src_files]
 ext_modules = [
     Extension(
         name=libpath,
