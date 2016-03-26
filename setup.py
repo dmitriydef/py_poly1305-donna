e433688a3f03b21c93e890604e1ecbb83d91d9cd
Sundar Nagarajan <sun.nagarajan@gmail.com>
2016-03-26 14:08:27 -0700

    0.11.13

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ PACKAGE DATA
 '''
 # You _SHOULD_ set these
 name = 'poly1305_donna'
-version = '0.11.12'   # oldver: '0.11.11'
+version = '0.11.13'   # oldver: '0.11.12'
 url = 'https://github.com/sundarnagarajan/py_poly1305-donna'
 download_url = '%s/tree/%s' % (url, version)
 description = name
